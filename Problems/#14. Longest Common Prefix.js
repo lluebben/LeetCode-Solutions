d745d635b59ae02ba472ba57b62622b2232abe3f
0II <jacklahvh@gmail.com>
2020-05-18 22:46:01 +0100

    :octocat:

---
+++ Problems/#14. Longest Common Prefix.js
@@ -0,0 +1,13 @@
+var longestCommonPrefix = function (strs) {
+    if (strs.length > 0) {
+      let minLen = Math.min(...strs.map(s => s.length))
+      const anyStr = strs[0]
+      while (minLen) {
+        const prefix = anyStr.slice(0, minLen--)
+        if (strs.every(s => s.startsWith(prefix))) {
+          return prefix
+        }
+      }
+    }
+    return ''
+  };
