9e408ca0ab47dd5ab37d8f654ffe3e69b6d9486b
0II <jacklahvh@gmail.com>
2020-05-18 22:50:50 +0100

    :octocat:

---
+++ Problems/#11. Container With Most Water.js
@@ -0,0 +1,14 @@
+var maxArea = function(height) {
+    let max = 0;
+    let l = 0;
+    let r = height.length - 1;
+    while (l < r) {
+      max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+      if (height[l] < height[r]) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+    return max;
+  };
