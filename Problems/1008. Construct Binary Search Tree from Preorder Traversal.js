5534629597d8ef40c1adc97833480173be5a2ca8
0II <jacklahvh@gmail.com>
2020-05-29 20:34:36 +0100

    :octocat:

---
+++ Problems/1008. Construct Binary Search Tree from Preorder Traversal.js
@@ -0,0 +1,21 @@
+var bstFromPreorder = function(preorder, start = 0, end = preorder.length) {
+    if (start >= end) {
+      return null;
+    }
+    const root = new TreeNode(preorder[start]);
+    const mid = findMid(preorder, start, end);
+    root.left = bstFromPreorder(preorder, start + 1, mid);
+    root.right = bstFromPreorder(preorder, mid, end);
+    return root;
+  };
+  
+  function findMid(arr, start, end) {
+    let i = start + 1;
+    while (i < end) {
+      if (arr[i] > arr[start]) {
+        return i;
+      }
+      i += 1;
+    }
+    return i;
+  }
