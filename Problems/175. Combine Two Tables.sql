9ac9f82177e057a4bf460768601791132277f678
Jack Latimer <jpwww@protonmail.com>
2020-06-16 15:41:42 +0100

    :octocat:

---
+++ Problems/175. Combine Two Tables.sql
@@ -0,0 +1,7 @@
+SELECT          FirstName,
+                LastName,
+                City,
+                State
+FROM            Person AS p
+LEFT JOIN       Address AS a
+ON              p.PersonId = a.PersonId
