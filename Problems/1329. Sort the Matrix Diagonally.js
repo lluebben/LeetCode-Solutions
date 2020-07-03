8d0ef6c33537ec4f6e957dee4dcf99c9498fc292
Jack Latimer <jpwww@protonmail.com>
2020-07-03 22:42:37 +0100

    :octocat:

---
+++ Problems/1329. Sort the Matrix Diagonally.js
@@ -0,0 +1,45 @@
+var diagonalSort = function (mat) {
+    let n = matrix.length;
+    let m = matrix[0].length;
+
+    for (let i = 0; i < n + m - 1; i++) {
+        let row = i < m ? 0 : i - m + 1;
+        let col = i < m ? i : m - 1;
+
+        helper(row, col);
+    }
+
+    for (let i = 1; i < n; i++) {
+        let row = i;
+        let col = 0;
+
+        helper(row, col);
+    }
+
+    function helper(row, col) {
+        let ans = [];
+        let x = row;
+        let y = col;
+
+        while (x < n && y < m) {
+            ans.push(matrix[x][y]);
+            x++;
+            y++;
+        }
+
+        x = row;
+        y = col;
+
+        let k = 0;
+        ans.sort((a, b) => a - b);
+
+        while (x < n && y < m && k < ans.length) {
+            matrix[x][y] = ans[k];
+            k++;
+            x++;
+            y++;
+        }
+    }
+
+    return matrix;
+};
